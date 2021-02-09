44b58a34ed03a92065b249196db05f2345cf5abe
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-02-09 15:36:56 +0530

    spinner added

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { useGlobalContext } from '../Context';
-import './stories.css'
+import './stories.css';
 import { Navbar, Nav, Form, FormControl, Button, Card } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 
@@ -8,12 +8,21 @@ const Stories = () => {
 	const { isLoading, hits } = useGlobalContext();
 
 	if (isLoading) {
-		return <div className='loading'></div>;
+		return (
+			<div>
+				<div class='centered'>
+					<div class='blob-1'></div>
+					<div class='blob-2'></div>
+				</div>
+			</div>
+		);
 	}
 	return (
 		<section className='stories'>
 			<Navbar bg='dark' variant='dark'>
-				<Navbar.Brand href='#home' className='icon'>News App</Navbar.Brand>
+				<Navbar.Brand href='#home' className='icon'>
+					News App
+				</Navbar.Brand>
 				<Nav className='mr-auto'></Nav>
 				<Form inline className='search-form'>
 					<FormControl type='text' placeholder='Search' className='mr-sm-2' />
@@ -25,28 +34,31 @@ const Stories = () => {
 
 				return (
 					<article className='story' key={objectID}>
-						  <Fade bottom >
-						<Card border='warning' style={{ width: '25rem' }}>
-							
-							<Card.Header className='card-header'>{title}</Card.Header>
-							<Card.Body>
-								<Card.Text>
-									<p>
-										{points} points by {author} |<span> {num_comments} comments </span>
-									</p>
-								</Card.Text>
-								<div>
-									<Button variant="dark" className='read-btn' href={url} target='_blank'>
-										Read More
-									</Button>{' '}
-									<Button variant="danger" className='remove-btn'>
-										Remove
-									</Button>
-								</div>
+						<Fade bottom>
+							<Card border='warning' style={{ width: '25rem' }}>
+								<Card.Header className='card-header'>{title}</Card.Header>
+								<Card.Body>
+									<Card.Text>
+										<p>
+											{points} points by {author} |
+											<span> {num_comments} comments </span>
+										</p>
+									</Card.Text>
+									<div>
+										<Button
+											variant='dark'
+											className='read-btn'
+											href={url}
+											target='_blank'>
+											Read More
+										</Button>{' '}
+										<Button variant='danger' className='remove-btn'>
+											Remove
+										</Button>
+									</div>
 								</Card.Body>
-								
 							</Card>
-							</Fade>
+						</Fade>
 					</article>
 				);
 			})}
